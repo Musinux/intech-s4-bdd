8702c347676a0779aabbf72d997a24de73c56b70
Louis Cherel <cherel.louis@gmail.com>
2020-09-28 12:16:42 +0200

    carts.route.js: fix hasToBeAuthenticated middleware

--- routes/carts.route.js
+++ routes/carts.route.js
@@ -2,16 +2,16 @@ const router = require('express').Router()
 const Cart = require('../models/cart.model')
 const hasToBeAuthenticated = require('../middlewares/has-to-be-authenticated.middleware')
 
-router.use(hasToBeAuthenticated)
+// router.use(hasToBeAuthenticated)
 
 // ne retourne que les paniers liés à l'utilisateur en cours
-router.get('/carts', async (req, res) => {
+router.get('/carts', hasToBeAuthenticated, async (req, res) => {
     const carts = await Cart.getByUserId(req.session.userId)
     res.json(carts)
 })
 
 // doit vérifier que l'utilisateur n'a pas déjà un panier non validé
-router.post('/cart', async (req, res) => {
+router.post('/cart', hasToBeAuthenticated, async (req, res) => {
     const hasCart = await Cart.getUserCurrentCart(req.session.userId)
     if (!hasCart) {
         const cart = await Cart.create(req.session.userId)
@@ -25,17 +25,17 @@ router.post('/cart', async (req, res) => {
 })
 
 // doit vérifier que l'utilisateur a déjà un panier
-router.put('/cart/:cartId', async (req, res) => {
+router.put('/cart/:cartId', hasToBeAuthenticated, async (req, res) => {
 
 })
 
-router.get('/cart/:cartId', async (req, res) => {
+router.get('/cart/:cartId', hasToBeAuthenticated, async (req, res) => {
 
 })
 
 // doit vérifier que le cart n'a pas déjà été validé
 // on ne peut pas supprimer une commande validée, sinon c'est la ruine
-router.delete('/cart/:cartId', async (req, res) => {
+router.delete('/cart/:cartId', hasToBeAuthenticated, async (req, res) => {
 
 })
 
